8ef73c42fb1366cf0ada4cfbc1e81b6fb2e52335
Tyler Wickline <twick00@users.noreply.github.com>
2018-03-05 14:28:02 -0800

    Create bazzr_test.sql

---
+++ bazzr_test.sql
@@ -0,0 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Mar 05, 2018 at 09:36 PM
+-- Server version: 5.6.35
+-- PHP Version: 7.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bazzr_test`
+--
+CREATE DATABASE IF NOT EXISTS `bazzr_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `bazzr_test`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buy_offer`
+--
+
+CREATE TABLE `buy_offer` (
+  `id` int(11) NOT NULL,
+  `offerer_id` int(11) NOT NULL,
+  `game_id` int(11) NOT NULL,
+  `transaction_sell_id` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game`
+--
+
+CREATE TABLE `game` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `platform` varchar(255) NOT NULL,
+  `game_description` varchar(255) NOT NULL,
+  `photo_path` varchar(255) NOT NULL,
+  `meta_score` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `games_tags`
+--
+
+CREATE TABLE `games_tags` (
+  `id` int(11) NOT NULL,
+  `game_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sell_transaction`
+--
+
+CREATE TABLE `sell_transaction` (
+  `id` int(11) NOT NULL,
+  `user_seller` int(11) NOT NULL,
+  `user_buyer` int(11) NOT NULL,
+  `game_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `photo_of_traded_game` varchar(255) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `filter_by_true_false` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `hashed_password` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8 COLLATE utf8_estonian_ci NOT NULL,
+  `rep` int(11) NOT NULL,
+  `date_registered` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buy_offer`
+--
+ALTER TABLE `buy_offer`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `offer_id` (`offerer_id`,`game_id`),
+  ADD KEY `game_id` (`game_id`);
+
+--
+-- Indexes for table `game`
+--
+ALTER TABLE `game`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `games_tags`
+--
+ALTER TABLE `games_tags`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `game_id` (`game_id`,`tag_id`),
+  ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Indexes for table `sell_transaction`
+--
+ALTER TABLE `sell_transaction`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_seller` (`user_seller`,`user_buyer`),
+  ADD KEY `user_buyer` (`user_buyer`),
+  ADD KEY `game_id` (`game_id`);
+
+--
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buy_offer`
+--
+ALTER TABLE `buy_offer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `game`
+--
+ALTER TABLE `game`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `games_tags`
+--
+ALTER TABLE `games_tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `sell_transaction`
+--
+ALTER TABLE `sell_transaction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `buy_offer`
+--
+ALTER TABLE `buy_offer`
+  ADD CONSTRAINT `buy_offer_ibfk_1` FOREIGN KEY (`offerer_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `buy_offer_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `games_tags`
+--
+ALTER TABLE `games_tags`
+  ADD CONSTRAINT `games_tags_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `games_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sell_transaction`
+--
+ALTER TABLE `sell_transaction`
+  ADD CONSTRAINT `sell_transaction_ibfk_1` FOREIGN KEY (`user_seller`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sell_transaction_ibfk_2` FOREIGN KEY (`user_buyer`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sell_transaction_ibfk_3` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
