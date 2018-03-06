84ff020b99e0a525eb07466f3c93c311dc97c02e
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-03-06 09:09:07 -0800

    initialize controllers.

---
+++ BazzR/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BazzR.Controllers
+{
+	public class UsersController : Controller
+	{
+		[HttpPost("/user/create/{id}")]
+        public ActionResult Create()
+        {
+            //read parameters from form
+            User newUser = new User(//parameters);
+            newUser.Save();
+            RedirectToAction("Index", "Home");
+        }
+
+	}
+}
