9c53d8d4760dc57fb7de937648f57adba13a80dc
Eric <eric.swotinsky@gmail.com>
2018-03-06 13:57:15 -0800

    build remaining Game methods.

--- BazzR/Models/Game.cs
+++ BazzR/Models/Game.cs
@@ -1,180 +1,277 @@
-// using System;
-// using System.Collections.Generic;
-// using Bazzr;
-// //using MySql.Data.MySqlClient;
-//
-// namespace Bazzr.Models
-// {
-//   public class Game
-//   {
-//     private int _id;
-//     private string _title;
-//     private string _platform;
-//     private string _description;
-//     private string _photopath;
-//     private int _metascore;
-//
-//     public Game(string title, string platform, string description, string photopath, int metascore, int id = 0)
-//     {
-//       _id = id;
-//       _title = title;
-//       _platform = platform;
-//       _description = description;
-//       _photoPath = photopath;
-//       _metaScore = metascore;
-//     }
-//
-//     public override bool Equals(System.Object otherGame)
-//     {
-//       if (!(otherGame is Game))
-//       {
-//         return false;
-//       }
-//       else
-//       {
-//         Game newGame = (Game) otherGame;
-//         return this.GetId().Equals(newGame.GetId());
-//       }
-//     }
-//
-//     public override int GetHashCode()
-//     {
-//       return this.GetId().GetHashCode();
-//     }
-//
-//     //_id getter/setter
-//     public int GetId()
-//     {
-//       return _id;
-//     }
-//     public void SetId(int newId)
-//     {
-//       _id = newId;
-//     }
-//
-//     //_title getter/setter
-//     public string GetTitle()
-//     {
-//       return _title;
-//     }
-//     public void SetTitle(string newTitle)
-//     {
-//       _title = newTitle;
-//     }
-//
-//     //_platform getter/setter
-//     public string GetPlatform()
-//     {
-//       return _platform;
-//     }
-//     public void SetPlatform(string newPlatform)
-//     {
-//       _platform = newPlatform;
-//     }
-//
-//     //_description getter/setter
-//     public string GetDescription()
-//     {
-//       return _description;
-//     }
-//     public void SetDescription(string newDescription)
-//     {
-//        MySqlConnection conn = DB.Connection();
-//        conn.Open();
-//
-//        var cmd = conn.CreateCommand() as MySqlCommand;
-//        cmd.CommandText = @"INSERT INTO games (title, platform, description, photopath, metascore) VALUES (@title, @platform, @description, @photopath, @metascore);";
-//
-//        MySqlParameter name = new MySqlParameter("@title, _title);
-//        cmd.Parameters.Add(title);
-//
-//        MySqlParameter platform = new MySqlParameter("@platform, _platform);
-//        cmd.Parameters.Add(platform);
-//
-//        MySqlParameter description = new MySqlParameter("@description, _description);
-//        cmd.Parameters.Add(description);
-//
-//        MySqlParameter photopath = new MySqlParameter("@photopath, _photopath);
-//        cmd.Parameters.Add(photopath);
-//
-//        MySqlParameter metascore = new MySqlParameter("@metascore, _metascore);
-//        cmd.Parameters.Add(metascore);
-//
-//        cmd.ExecuteNonQuery();
-//        _id = (int) cmd.LastInsertedId;
-//        conn.Close();
-//
-//        if (conn != null)
-//        {
-//            conn.Dispose();
-//        }
-//     }
-//
-//     //_photopath getter/setter
-//     public string GetPhotoPath()
-//     {
-//       return _photopath;
-//     }
-//     public void SetPhotoPath(string newPhotoPath)
-//     {
-//       _photopath = newPhotoPath;
-//     }
-//
-//     //_metascore getter/setter
-//     public int GetMetaScore()
-//     {
-//       return _metascore;
-//     }
-//     public void SetMetaScore(int newMetaScore)
-//     {
-//       _metascore = newMetaScore;
-//     }
-//
-//     public void Save()
-//   {
-// -      //save game to database
-// +       MySqlConnection conn = DB.Connection();
-// +       conn.Open();
-// +
-// +       var cmd = conn.CreateCommand() as MySqlCommand;
-// +       cmd.CommandText = @"INSERT INTO games (title, platform, description, photopath, metascore)
-//         VALUES (@title, @platform, @description, @photopath, @metascore);";
-// +
-// +       MySqlParameter name = new MySqlParameter("@title", _title);
-// +       cmd.Parameters.Add(title);
-// +
-// +       MySqlParameter platform = new MySqlParameter("@platform", _platform);
-// +       cmd.Parameters.Add(platform);
-// +
-// +       MySqlParameter description = new MySqlParameter("@description", _description);
-// +       cmd.Parameters.Add(description);
-// +
-// +       MySqlParameter photopath = new MySqlParameter("@photopath", _photopath);
-// +       cmd.Parameters.Add(photopath);
-// +
-// +       MySqlParameter metascore = new MySqlParameter("@metascore", _metascore);
-// +       cmd.Parameters.Add(metascore);
-// +
-// +       cmd.ExecuteNonQuery();
-// +       _id = (int) cmd.LastInsertedId;
-// +       conn.Close();
-// +
-// +       if (conn != null)
-// +       {
-// +           conn.Dispose();
-// +       }
-//   }
-//
-//
-//
-//     public static List<Game> GetAll()
-//     {
-//       //update - get all games from database
-//       List<Game> allGames = new List<Game>{};
-//       return allGames;
-//     }
-//
-//     //public void Edit(string newTitle, etc.)
-//     //edit title / other options in database
-//   }
-// }
+using System;
+using System.Collections.Generic;
+using Bazzr;
+//using MySql.Data.MySqlClient;
+
+namespace Bazzr.Models
+{
+    public class Game
+    {
+        private int _id;
+        private string _title;
+        private string _platform;
+        private string _description;
+        private string _photopath;
+        private int _metascore;
+
+        public Game(string title, string platform, string description, string photopath, int metascore, int id = 0)
+        {
+            _id = id;
+            _title = title;
+            _platform = platform;
+            _description = description;
+            _photoPath = photopath;
+            _metaScore = metascore;
+        }
+
+        public override bool Equals(System.Object otherGame)
+        {
+            if (!(otherGame is Game))
+            {
+                return false;
+            }
+            else
+            {
+                Game newGame = (Game) otherGame;
+                return this.GetId().Equals(newGame.GetId());
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.GetId().GetHashCode();
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public void SetId(int newId)
+        {
+            _id = newId;
+        }
+
+        public string GetTitle()
+        {
+            return _title;
+        }
+
+        public void SetTitle(string newTitle)
+        {
+            _title = newTitle;
+        }
+
+        public string GetPlatform()
+        {
+            return _platform;
+        }
+
+        public void SetPlatform(string newPlatform)
+        {
+            _platform = newPlatform;
+        }
+
+        public string GetDescription()
+        {
+            return _description;
+        }
+
+        public void SetDescription(string newDescription)
+        {
+            _description = newDescription;
+        }
+
+        public string GetPhotoPath()
+        {
+            return _photopath;
+        }
+
+        public void SetPhotoPath(string newPhotopath)
+        {
+            _photopath = newPhotopath;
+        }
+
+        public int GetMetaScore()
+        {
+            return _metascore;
+        }
+
+        public void SetMetascore(int newMetascore)
+        {
+            _metascore = newMetascore;
+        }
+
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO games (title, platform, description, photopath, metascore)
+            VALUES (@title, @platform, @description, @photopath, @metascore);";
+
+            MySqlParameter name = new MySqlParameter("@title", _title);
+            cmd.Parameters.Add(title);
+
+            MySqlParameter platform = new MySqlParameter("@platform", _platform);
+            cmd.Parameters.Add(platform);
+
+            MySqlParameter description = new MySqlParameter("@description", _description);
+            cmd.Parameters.Add(description);
+
+            MySqlParameter photopath = new MySqlParameter("@photopath", _photopath);
+            cmd.Parameters.Add(photopath);
+
+            MySqlParameter metascore = new MySqlParameter("@metascore", _metascore);
+            cmd.Parameters.Add(metascore);
+
+            cmd.ExecuteNonQuery();
+            _id = (int) cmd.LastInsertedId;
+            conn.Close();
+
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public static Game Find(int id)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"SELECT * FROM games WHERE id = @searchId;";
+
+            MySqlParameter searchId = new MySqlParameter("@searchId", id);
+            cmd.Parameters.Add(searchId);
+
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+
+            int gameId = 0;
+            string gameTitle = "";
+            string gamePlatform = "";
+            string gameDescription = "";
+            string gamePhotopath = "";
+            int gameMetascore = 0;
+
+            while(rdr.Read())
+            {
+                gameId = rdr.GetInt32(0);
+                gameTitle = rdr.GetString(1);
+                gamePlatform = rdr.GetString(2);
+                gameDescription = rdr.GetString(3);
+                gamePhotopath = rdr.GetString(4);
+                gameMetascore = rdr.GetInt32(5);
+            }
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+
+            Game myGame = new Game(gameTitle, gamePlatform, gameDescription, gamePhotopath, gameMetascore, gameId);
+            return myGame;
+        }
+
+        public static List<Game> GetAll()
+        {
+            List<Game> allGames = new List<Game>{};
+            MySqlConnection conn = DB.Connection();
+
+            conn.Open();
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while(rdr.Read());
+            {
+                gameId = rdr.GetInt32(0);
+                gameTitle = rdr.GetString(1);
+                gamePlatform = rdr.GetString(2);
+                gameDescription = rdr.GetString(3);
+                gamePhotopath = rdr.GetString(4);
+                gameMetascore = rdr.GetInt32(5);
+                Game newGame = new Game(gameTitle, gamePlatform, gameDescription, gamePhotopath, gameMetascore, gameId);
+                allGames.Add(newGame);
+            }
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+
+            return allGames;
+        }
+
+        public void Edit(string newTitle, string newPlatform, string newDescription, string newPhotopath, string newMetascore)
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"UPDATE games SET title = @newTitle, platform = @newPlatform, description = @newDescription, photopath = @newPhotopath, metascore = @newMetascore WHERE id=@id;";
+
+            MySqlParameter updateTitle = new MySqlParameter("@newTitle", newTitle);
+            MySqlParameter updatePlatform = new MySqlParameter("@newPlatform", newPlatform);
+            MySqlParameter updateDescription = new MySqlParameter("@newDescription", newDescription);
+            MySqlParameter updatePhotopath = new MySqlParameter("@newPhotopath", newPhotopath);
+            MySqlParameter updateMetascore = new MySqlParameter("@newMetascore", newMetascore);
+            cmd.Parameters.Add(updateTitle);
+            cmd.Parameters.Add(updatePlatform);
+            cmd.Parameters.Add(updateDescription);
+            cmd.Parameters.Add(updatePhotopath);
+            cmd.Parameters.Add(updateMetascore);
+
+            cmd.ExecuteNonQuery();
+            _title = newTitle;
+            _platform = newPlatform;
+            _description = newDescription;
+            _photopath = newPhotopath;
+            _metascore = newMetascore;
+
+            conn.Close();
+            if(conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public void Delete()
+        {
+            MySqlConnection cmd = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM games WHERE id = @thisId;";
+
+            MySqlParameter searchId = newMySqlParameter("@thisId", _id);
+            cmd.Parameters.Add(searchId);
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if(conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public static void DeleteAll()
+        {
+            MySqlConnection cmd = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"DELETE FROM games;";
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if(conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+    }
+}
