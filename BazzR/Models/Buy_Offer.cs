613c3ffea99f2d21464c76078c031b9743fc348a
Drew Tucker <dtuck43@gmail.com>
2018-03-06 12:05:29 -0800

    Update Buy_Offer.cs

--- BazzR/Models/Buy_Offer.cs
+++ BazzR/Models/Buy_Offer.cs
@@ -1,84 +1,5 @@
 using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
-using BazzrApp;
-using MySql.Data.MySqlClient;
-
-namespace BazzrApp.Models
-{
-  public class Buy_Offer
-  {
-    private int _id;
-    private int _gameId;
-    private int _userIdBuyer;
-    private int _transaction_SellId;
-    private DateTime _date;
-
-    public Buy_Offer(int game_id, DateTime date, int user_id_buyer, int transaction_Sell_id = 0, int id = 0)
-    {
-      _id = id;
-      _gameId = game_id;
-      _userIdBuyer = user_id_buyer;
-      _date = date;
-      _transaction_SellId = transaction_Sell_id;
-    }
-
-    public override bool Equals(System.Object otherBuy_Offer)
-    {
-      if (!(otherBuy_Offer is Buy_Offer))
-      {
-        return false;
-      }
-      else
-      {
-        Buy_Offer newBuy_Offer = (Buy_Offer) otherBuy_Offer;
-        return this.GetId().Equals(newBuy_Offer.GetId());
-      }
-    }
-
-    public override int GetHashCode()
-    {
-      return this.GetId().GetHashCode();
-    }
-
-    public int GetId()
-    {
-      return _id;
-    }
-
-    public int GetGameId()
-    {
-      return _gameId;
-    }
-    public int GetUserIdBuyer()
-    {
-      return _userIdBuyer;
-    }
-    public DateTime GetDate()
-    {
-      return _date;
-    }
-    public int GetTransaction_SellId()
-    {
-      return _transaction_SellId;
-    }
-
-    public void Save()
-    {
-      //save game to database
-    }
-
-    public static List<Buy_Offer> GetAll()
-    {
-      //update - get all games from database
-      List<Buy_Offer> allBuy_Offer = new List<Buy_Offer>{};
-      return allBuy_Offer;
-    }
-
-    //public void Edit(string newTitle, etc.)
-    //edit title / other options in database
-  }
-=======
 using Bazzr;
 using MySql.Data.MySqlClient;
 
@@ -260,5 +181,4 @@ namespace Bazzr.Models
       //public void Edit(string newTitle, etc.)
       //edit title / other options in database
     }
->>>>>>> c6ba035983ffa826d215aa893dc7a5e4ac8ea062
 }
