d51b93c103219338e8729ec9509f9feb9a8fed8c
Josh <joshdrochon@gmail.com>
2018-03-06 10:29:51 -0800

    Update Game.cs

--- BazzR/Models/Game.cs
+++ BazzR/Models/Game.cs
@@ -1,21 +1,27 @@
 using System;
 using System.Collections.Generic;
-using BazzrApp;
-using MySql.Data.MySqlClient;
+using Bazzr;
+//using MySql.Data.MySqlClient;
 
-namespace BazzrApp.Models
+namespace Bazzr.Models
 {
   public class Game
   {
     private int _id;
     private string _title;
     private string _platform;
+    private string _description;
+    private string _photopath;
+    private int _metascore;
 
-    public Game(string title, string platform, int id = 0)
+    public Game(string title, string platform, string description, string photopath, int metascore, int id = 0)
     {
       _id = id;
       _title = title;
       _platform = platform;
+      _description = description;
+      _photoPath = photopath;
+      _metaScore = metascore;
     }
 
     public override bool Equals(System.Object otherGame)
@@ -36,20 +42,102 @@ namespace BazzrApp.Models
       return this.GetId().GetHashCode();
     }
 
-    public string GetTitle()
-    {
-      return _title;
-    }
-
+    //_id getter/setter
     public int GetId()
     {
       return _id;
     }
+    public void SetId(int newId)
+    {
+      _id = newId;
+    }
+
+    //_title getter/setter
+    public string GetTitle()
+    {
+      return _title;
+    }
+    public void SetTitle(string newTitle)
+    {
+      _title = newTitle;
+    }
+
+    //_platform getter/setter
+    public string GetPlatform()
+    {
+      return _platform;
+    }
+    public void SetPlatform(string newPlatform)
+    {
+      _platform = newPlatform;
+    }
+
+    //_description getter/setter
+    public string GetDescription()
+    {
+      return _description;
+    }
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+
+    //_photopath getter/setter
+    public string GetPhotoPath()
+    {
+      return _photopath;
+    }
+    public void SetPhotoPath(string newPhotoPath)
+    {
+      _photopath = newPhotoPath;
+    }
+
+    //_metascore getter/setter
+    public int GetMetaScore()
+    {
+      return _metascore;
+    }
+    public void SetMetaScore(int newMetaScore)
+    {
+      _metascore = newMetaScore;
+    }
 
     public void Save()
-    {
-      //save game to database
-    }
+  {
+-      //save game to database
++       MySqlConnection conn = DB.Connection();
++       conn.Open();
++
++       var cmd = conn.CreateCommand() as MySqlCommand;
++       cmd.CommandText = @"INSERT INTO games (title, platform, description, photopath, metascore)
+        VALUES (@title, @platform, @description, @photopath, @metascore);";
++
++       MySqlParameter name = new MySqlParameter("@title", _title);
++       cmd.Parameters.Add(title);
++
++       MySqlParameter platform = new MySqlParameter("@platform", _platform);
++       cmd.Parameters.Add(platform);
++
++       MySqlParameter description = new MySqlParameter("@description", _description);
++       cmd.Parameters.Add(description);
++
++       MySqlParameter photopath = new MySqlParameter("@photopath", _photopath);
++       cmd.Parameters.Add(photopath);
++
++       MySqlParameter metascore = new MySqlParameter("@metascore", _metascore);
++       cmd.Parameters.Add(metascore);
++
++       cmd.ExecuteNonQuery();
++       _id = (int) cmd.LastInsertedId;
++       conn.Close();
++
++       if (conn != null)
++       {
++           conn.Dispose();
++       }
+  }
+
+
 
     public static List<Game> GetAll()
     {
