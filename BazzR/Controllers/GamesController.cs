6aad066bd7e5f09bac565674fcc7e14c368faf82
Josh Rochon <joshdrochon@gmail.com>
2018-03-07 10:02:04 -0800

    add two routes in game controller add NewGameForm

--- BazzR/Controllers/GamesController.cs
+++ BazzR/Controllers/GamesController.cs
@@ -1,21 +1,33 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using Microsoft.AspNetCore.Mvc;
-//
-// namespace BazzR.Controllers
-// {
-//     public class GamesController : Controller
-//     {
-//         [HttpPost("/game/create")]
-//         public ActionResult Create()
-//         {
-//             string gameName = Request.Form["game-title"];
-//             string gamePlatform = Request.Form["game-platform"];
-//             Game newGame = new Game(gameName, gamePlatform);
-//             newGame.Save();
-//             return RedirectToAction("NewGameForm");
-//         }
-//     }
-// }
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Bazzr.Models;
+using Bazzr;
+
+namespace Bazzr.Controllers
+{
+    public class GamesController : Controller
+    {
+      [HttpGet("/game/new")]
+      public ActionResult NewGameForm()
+      {
+        return View();
+      }
+
+      [HttpPost("/game")]
+        public ActionResult Create()
+        {
+          string gameName = Request.Form["game-title"];
+          string gamePlatform = Request.Form["game-platform"];
+          string gameDescription = Request.Form["game-description"];
+          string gamePhotopath = Request.Form["game-photopath"];
+          int gameMetascore = int.Parse(Request.Form["game-metascore"]);
+          Game newGame = new Game(gameName, gamePlatform, gameDescription, gamePhotopath, gameMetascore);
+          newGame.Save();
+          // return RedirectToAction("UserProfile");
+          return RedirectToAction("NewGameForm");
+        }
+    }
+}
