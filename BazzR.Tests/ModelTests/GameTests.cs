f679ce8bcfd70b5e029288ffb04aa12ba7057bb0
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-03-07 09:26:08 -0800

    add tag-related methods to Game.cs; add Game tests.

--- BazzR.Tests/ModelTests/GameTests.cs
+++ BazzR.Tests/ModelTests/GameTests.cs
@@ -5,74 +5,110 @@ using Bazzr.Models;
 
 namespace Bazzr.Tests
 {
-//     [TestClass]
-//     public class GameTests : IDisposable
-//     {
-//         public GameTests()
-//         {
-//             DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=bazzr;";
-//         }
+    [TestClass]
+    public class GameTests : IDisposable
+    {
+        public GameTests()
+        {
+            DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=bazzr_test;";
+        }
 
-//         public void Dispose()
-//         {
-//             User.DeleteAll();
-//             Game.DeleteAll();
-//             Tag.DeleteAll();
-//         }
+        public void Dispose()
+        {
+            //User.DeleteAll();
+            Game.DeleteAll();
+            Tag.DeleteAll();
+        }
 
-//         [TestMethod]
-//         public void GetName_ReturnsGameName_String()
-//         {
-//             string testName = "Super Mario World";
-//             Game testGame = new Game(testName, "SNES");
+        [TestMethod]
+        public void GetName_ReturnsGameName_String()
+        {
+            string testName = "Super Mario World";
+            Game testGame = new Game(testName, "SNES", "", "", 0);
 
-//             string result = testGame.GetName();
+            string result = testGame.GetTitle();
 
-//             Assert.AreEqual(testName, result);
-//         }
+            Assert.AreEqual(testName, result);
+        }
 
-//         [TestMethod]
-//         public void GetAll_GamesEmptyAtFirst_0()
-//         {
-//            int result = Game.GetAll().Count;
-//            Assert.AreEqual(0, result);
-//         }
+        [TestMethod]
+        public void GetAll_GamesEmptyAtFirst_0()
+        {
+           int result = Game.GetAll().Count;
+           Assert.AreEqual(0, result);
+        }
 
-//         [TestMethod]
-//         public void GetAll_ReturnsAllGames_GameList()
-//         {
-//             Game newGame1 = new Game("Super Mario World", "SNES");
-//             Game newGame2 = new Game("Zombies Ate My Neighbors", "SNES");
+        [TestMethod]
+        public void GetAll_ReturnsAllGames_GameList()
+        {
+            Game testGame1 = new Game("Super Mario World", "SNES", "", "", 0);
+            Game testGame2 = new Game("Zombies Ate My Neighbors", "SNES", "", "", 0);
 
-//             newGame1.Save();
-//             newGame2.Save();
+            testGame1.Save();
+            testGame2.Save();
 
-//             List<Game> newList = new List<Game> {newGame1, newGame2};
-//             List<Game> result = Game.GetAll();
-//             CollectionAssert.AreEqual(newList, result);
-//         }
+            List<Game> testList = new List<Game> {testGame1, testGame2};
+            List<Game> result = Game.GetAll();
+            CollectionAssert.AreEqual(testList, result);
+        }
 
-//         [TestMethod]
-//         public void Save_SavesGameToDatabase_GameList()
-//         {
-//             Game newGame = new Game("Super Mario World", "SNES");
-//             testGame.Save();
+        [TestMethod]
+        public void Save_SavesGameToDatabase_GameList()
+        {
+            Game testGame = new Game("Super Mario World", "SNES", "", "", 0);
+            testGame.Save();
 
-//             List<Game> testList = new List<Game>{testGame};
-//             List<Game> resultList = Game.GetAll();
+            List<Game> testList = new List<Game>{testGame};
+            List<Game> resultList = Game.GetAll();
 
-//             CollectionAssert.AreEqual(testList, resultList);
-//         }
+            CollectionAssert.AreEqual(testList, resultList);
+        }
 
-//         [TestMethod]
-//         public void Find_FindsGameInDatabase_Game()
-//         {
-//             Game newGame = new Game("Super Mario World", "SNES");
-//             testGame.Save();
+        [TestMethod]
+        public void Find_FindsGameInDatabase_Game()
+        {
+            Game testGame = new Game("Super Mario World", "SNES", "", "", 0);
+            testGame.Save();
 
-//             Game foundGame = Game.Find(testGame.GetId());
+            Game foundGame = Game.Find(testGame.GetId());
 
-//             Assert.AreEqual(testGame, foundGame);
-//         }
-//     }
+            Assert.AreEqual(testGame, foundGame);
+        }
+
+        [TestMethod]
+        public void GetTags_RetrievesAllTagsAssociatedWithGame_TagList()
+        {
+            Game testGame = new Game("Super Mario World", "SNES", "", "", 0);
+            testGame.Save();
+
+            Tag newTag1 = new Tag("Action");
+            Tag newTag2 = new Tag("Adventure");
+            newTag1.Save();
+            newTag2.Save();
+            testGame.AddTag(newTag1);
+            testGame.AddTag(newTag2);
+
+            List<Tag> testTagList = new List<Tag> {newTag1, newTag2};
+            List<Tag> resultTagList = testGame.GetTags();
+
+            CollectionAssert.AreEqual(testTagList, resultTagList);
+        }
+
+        [TestMethod]
+        public void AddTag_AddsTagAssociationToDatabase_TagList()
+        {
+            Game testGame = new Game("Super Mario World", "SNES", "", "", 0);
+            testGame.Save();
+
+            Tag newTag = new Tag("Action");
+            newTag.Save();
+
+            testGame.AddTag(newTag);
+
+            List<Tag> testList = new List<Tag>{newTag};
+            List<Tag> result = testGame.GetTags();
+
+            CollectionAssert.AreEqual(testList, result);
+        }
+    }
 }
