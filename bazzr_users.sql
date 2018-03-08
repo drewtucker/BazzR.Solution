604ee847412207d4b4c4935818241ea04ec049d7
Tyler Wickline <twick00@users.noreply.github.com>
2018-03-08 13:15:49 -0800

    Create bazzr_users.sql

---
+++ bazzr_users.sql
@@ -0,0 +1,243 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Mar 08, 2018 at 10:15 PM
+-- Server version: 5.6.35
+-- PHP Version: 7.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `bazzr_users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `AspNetRoleClaims`
+--
+
+CREATE TABLE `AspNetRoleClaims` (
+  `Id` int(11) NOT NULL,
+  `ClaimType` longtext,
+  `ClaimValue` longtext,
+  `RoleId` varchar(127) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `AspNetRoles`
+--
+
+CREATE TABLE `AspNetRoles` (
+  `Id` varchar(127) NOT NULL,
+  `ConcurrencyStamp` longtext,
+  `Name` varchar(127) DEFAULT NULL,
+  `NormalizedName` varchar(127) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `AspNetUserClaims`
+--
+
+CREATE TABLE `AspNetUserClaims` (
+  `Id` int(11) NOT NULL,
+  `ClaimType` longtext,
+  `ClaimValue` longtext,
+  `UserId` varchar(127) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `AspNetUserLogins`
+--
+
+CREATE TABLE `AspNetUserLogins` (
+  `LoginProvider` varchar(127) NOT NULL,
+  `ProviderKey` varchar(127) NOT NULL,
+  `ProviderDisplayName` longtext,
+  `UserId` varchar(127) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `AspNetUserRoles`
+--
+
+CREATE TABLE `AspNetUserRoles` (
+  `UserId` varchar(127) NOT NULL,
+  `RoleId` varchar(127) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `AspNetUsers`
+--
+
+CREATE TABLE `AspNetUsers` (
+  `Id` varchar(127) NOT NULL,
+  `AccessFailedCount` int(11) NOT NULL,
+  `ConcurrencyStamp` longtext,
+  `Email` varchar(127) DEFAULT NULL,
+  `EmailConfirmed` bit(1) NOT NULL,
+  `LockoutEnabled` bit(1) NOT NULL,
+  `LockoutEnd` datetime(6) DEFAULT NULL,
+  `NormalizedEmail` varchar(127) DEFAULT NULL,
+  `NormalizedUserName` varchar(127) DEFAULT NULL,
+  `PasswordHash` longtext,
+  `PhoneNumber` longtext,
+  `PhoneNumberConfirmed` bit(1) NOT NULL,
+  `SecurityStamp` longtext,
+  `TwoFactorEnabled` bit(1) NOT NULL,
+  `UserName` varchar(127) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `AspNetUsers`
+--
+
+INSERT INTO `AspNetUsers` (`Id`, `AccessFailedCount`, `ConcurrencyStamp`, `Email`, `EmailConfirmed`, `LockoutEnabled`, `LockoutEnd`, `NormalizedEmail`, `NormalizedUserName`, `PasswordHash`, `PhoneNumber`, `PhoneNumberConfirmed`, `SecurityStamp`, `TwoFactorEnabled`, `UserName`) VALUES
+('e37196f0-ca48-44f4-ac50-413610113a31', 0, '98ee6886-7448-4e65-b1a6-808dac7f7a59', NULL, b'0', b'1', NULL, NULL, 'TWICK00@GMAIL.COM', 'AQAAAAEAACcQAAAAEOl079UmbyLr76YZ0uisJTgtGBUMSrkpKEPoFUrArLqCubbtR5QOgf7zPigq4CkAMQ==', NULL, b'0', 'de72b352-f121-43b3-8c09-a2717c6e99ca', b'0', 'Twick00@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `AspNetUserTokens`
+--
+
+CREATE TABLE `AspNetUserTokens` (
+  `UserId` varchar(127) NOT NULL,
+  `LoginProvider` varchar(127) NOT NULL,
+  `Name` varchar(127) NOT NULL,
+  `Value` longtext
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `__EFMigrationsHistory`
+--
+
+CREATE TABLE `__EFMigrationsHistory` (
+  `MigrationId` varchar(95) NOT NULL,
+  `ProductVersion` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `__EFMigrationsHistory`
+--
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`) VALUES
+('20180306231012_initial', '1.1.3'),
+('20180307214445_AddMoreUserFields', '1.1.3');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `AspNetRoleClaims`
+--
+ALTER TABLE `AspNetRoleClaims`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `IX_AspNetRoleClaims_RoleId` (`RoleId`);
+
+--
+-- Indexes for table `AspNetRoles`
+--
+ALTER TABLE `AspNetRoles`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `RoleNameIndex` (`NormalizedName`);
+
+--
+-- Indexes for table `AspNetUserClaims`
+--
+ALTER TABLE `AspNetUserClaims`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `IX_AspNetUserClaims_UserId` (`UserId`);
+
+--
+-- Indexes for table `AspNetUserLogins`
+--
+ALTER TABLE `AspNetUserLogins`
+  ADD PRIMARY KEY (`LoginProvider`,`ProviderKey`),
+  ADD KEY `IX_AspNetUserLogins_UserId` (`UserId`);
+
+--
+-- Indexes for table `AspNetUserRoles`
+--
+ALTER TABLE `AspNetUserRoles`
+  ADD PRIMARY KEY (`UserId`,`RoleId`),
+  ADD KEY `IX_AspNetUserRoles_RoleId` (`RoleId`);
+
+--
+-- Indexes for table `AspNetUsers`
+--
+ALTER TABLE `AspNetUsers`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `UserNameIndex` (`NormalizedUserName`),
+  ADD KEY `EmailIndex` (`NormalizedEmail`);
+
+--
+-- Indexes for table `AspNetUserTokens`
+--
+ALTER TABLE `AspNetUserTokens`
+  ADD PRIMARY KEY (`UserId`,`LoginProvider`,`Name`);
+
+--
+-- Indexes for table `__EFMigrationsHistory`
+--
+ALTER TABLE `__EFMigrationsHistory`
+  ADD PRIMARY KEY (`MigrationId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `AspNetRoleClaims`
+--
+ALTER TABLE `AspNetRoleClaims`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `AspNetUserClaims`
+--
+ALTER TABLE `AspNetUserClaims`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `AspNetRoleClaims`
+--
+ALTER TABLE `AspNetRoleClaims`
+  ADD CONSTRAINT `FK_AspNetRoleClaims_AspNetRoles_RoleId` FOREIGN KEY (`RoleId`) REFERENCES `AspNetRoles` (`Id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `AspNetUserClaims`
+--
+ALTER TABLE `AspNetUserClaims`
+  ADD CONSTRAINT `FK_AspNetUserClaims_AspNetUsers_UserId` FOREIGN KEY (`UserId`) REFERENCES `AspNetUsers` (`Id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `AspNetUserLogins`
+--
+ALTER TABLE `AspNetUserLogins`
+  ADD CONSTRAINT `FK_AspNetUserLogins_AspNetUsers_UserId` FOREIGN KEY (`UserId`) REFERENCES `AspNetUsers` (`Id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `AspNetUserRoles`
+--
+ALTER TABLE `AspNetUserRoles`
+  ADD CONSTRAINT `FK_AspNetUserRoles_AspNetRoles_RoleId` FOREIGN KEY (`RoleId`) REFERENCES `AspNetRoles` (`Id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_AspNetUserRoles_AspNetUsers_UserId` FOREIGN KEY (`UserId`) REFERENCES `AspNetUsers` (`Id`) ON DELETE CASCADE;
