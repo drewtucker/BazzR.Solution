efe769bd6ba856b7f6805104df0a428f7b3b443f
Tyler Wickline <Twick00@gmail.com>
2018-03-08 13:31:18 -0800

    Adds mostly full functionality to login system.

--- BazzR/Controllers/SellsController.cs
+++ BazzR/Controllers/SellsController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Bazzr.Models;
 
-namespace BazzR.Controllers
+namespace Bazzr.Controllers
 {
 	public class SellsController : Controller
 	{
 		[HttpGet("/trade/new")]
 		public IActionResult Index()
 		{
+			User thisUser = Bazzr.Models.User.Find(User.Identity.Name);
 			return View("WantToSell");
 		}
   }
--- BazzR/Models/ApplicationDbContext.cs
+++ BazzR/Models/ApplicationDbContext.cs
@@ -6,7 +6,6 @@ namespace BasicAuthentication.Models
 {
 	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 	{
-        public DbSet<User> Users { get; set; }
 		public ApplicationDbContext(DbContextOptions options) : base(options)
 		{
 
